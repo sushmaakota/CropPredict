7e4dbdf6f9d7a674b6d0e1be8c453abf9d52900e
sushmaakota <118443281+sushmaakota@users.noreply.github.com>
2024-09-15 21:43:37 +0530

    webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,6 @@
 ## Importing necessary libraries for the web app
 import streamlit as st
-# import numpy as np
+import numpy as np
 # import pandas as pd
 # from sklearn.ensemble import RandomForestClassifier
 import pickle
