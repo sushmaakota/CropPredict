1cd51bf121dcdedacd619df854896cf806b6a04f
sushmaakota <118443281+sushmaakota@users.noreply.github.com>
2024-09-16 09:31:51 +0530

    webapp.py

--- webapp.py
+++ webapp.py
@@ -4,72 +4,49 @@ import pickle
 import numpy as np
 import pandas as pd
 import os
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 import warnings
-warnings.filterwarnings("ignore", message="Trying to unpickle estimator")
+warnings.filterwarnings('ignore')
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-# import pandas as pd
-# from sklearn.ensemble import RandomForestClassifier
-# import os
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# from sklearn.metrics import classification_report
-# from sklearn import metrics
-# from sklearn import tree
-# from sklearn.metrics import accuracy_score
-# import warnings
-# warnings.filterwarnings('ignore')
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import LabelEncoder
+df= pd.read_csv('Crop_data.csv')
 
-# df= pd.read_csv('Crop_recommendation.csv')
+#features = df[['temperature', 'humidity', 'ph', 'rainfall']]
+X = df[['N', 'P','K','temperature', 'humidity', 'ph', 'rainfall']]
+y = df['label']
+labels = df['label']
 
-# #features = df[['temperature', 'humidity', 'ph', 'rainfall']]
-# X = df[['N', 'P','K','temperature', 'humidity', 'ph', 'rainfall']]
-# y = df['label']
-# labels = df['label']
+# Split the data into training and testing sets
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, y, test_size=0.3, random_state=42)
+RF = RandomForestClassifier(n_estimators=20, random_state=5)
+RF.fit(Xtrain,Ytrain)
+predicted_values = RF.predict(Xtest)
+x = metrics.accuracy_score(Ytest, predicted_values)
 
-# # Split the data into training and testing sets
-# Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, y, test_size=0.3, random_state=42)
-# RF = RandomForestClassifier(n_estimators=20, random_state=5)
-# RF.fit(Xtrain,Ytrain)
-# predicted_values = RF.predict(Xtest)
-# x = metrics.accuracy_score(Ytest, predicted_values)
+import pickle
+# Dump the trained Naive Bayes classifier with Pickle
+RF_pkl_filename = 'RF.pkl'
+# Open the file to save as pkl file
+RF_Model_pkl = open(RF_pkl_filename, 'wb')
+pickle.dump(RF, RF_Model_pkl)
+# Close the pickle instances
+RF_Model_pkl.close()
 
-# import pickle
-# # Dump the trained Naive Bayes classifier with Pickle
-# RF_pkl_filename = 'RF.pkl'
-# # Open the file to save as pkl file
-# RF_Model_pkl = open(RF_pkl_filename, 'wb')
-# pickle.dump(RF, RF_Model_pkl)
-# # Close the pickle instances
-# RF_Model_pkl.close()
-
-
-
-# Check if the model file exists before loading
-if os.path.exists('RandomForest.pkl'):
-    with open('RandomForest.pkl', 'rb') as model_file:
-        RF_Model_pkl = pickle.load(model_file)
-else:
-    st.error("Model file not found. Please ensure 'RandomForest.pkl' is in the correct directory.")
+model = pickle.load(open('RandomForest.pkl', 'rb'))
+RF_Model_pkl=pickle.load(open('RandomForest.pkl','rb'))
 
 ## Function to make predictions
 def predict_crop(nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall):
-    try:
-        prediction = RF_Model_pkl.predict(np.array([nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall]).reshape(1, -1))
-        return prediction
-    except Exception as e:
-        st.error(f"An error occurred during prediction: {str(e)}")
-        return None
-
-# model = pickle.load(open('RandomForest.pkl', 'rb'))
-# RF_Model_pkl=pickle.load(open('RandomForest.pkl','rb'))
-
-# ## Function to make predictions
-# def predict_crop(nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall):
-#     # # Making predictions using the model
-#     prediction = RF_Model_pkl.predict(np.array([nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall]).reshape(1, -1))
-#     return prediction
+    # # Making predictions using the model
+    prediction = RF_Model_pkl.predict(np.array([nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall]).reshape(1, -1))
+    return prediction
 
 ## Streamlit code for the web app interface
 def main():  
